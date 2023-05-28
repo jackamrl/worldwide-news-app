d555295564d2e823ec0d2b114a1024e43a39e48c
Londou Jacques BOYODI <88946894+jackamrl@users.noreply.github.com>
2023-05-28 23:03:43 +0000

    add menu on footer

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -3,13 +3,50 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  isDark!: boolean;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    const storedIsDarkMode = localStorage.getItem('isDarkMode');
+    if (storedIsDarkMode) {
+      this.isDark = JSON.parse(storedIsDarkMode);
+    } else {
+      // Default to light mode if no preference is stored
+      this.isDark = false;
+    }
+    this.applyTheme();
+  }
+  toggleDarkMode() {
+    this.isDark = !this.isDark;
+    localStorage.setItem('isDarkMode', JSON.stringify(this.isDark));
+    this.applyTheme();
   }
 
+  applyTheme() {
+    const darkClassName = 'dark-mode';
+    const lightClassName = 'light-mode';
+    const body = document.getElementsByTagName('body')[0];
+
+    if (this.isDark) {
+      body.classList.add(darkClassName);
+      body.classList.remove(lightClassName);
+    } else {
+      body.classList.add(lightClassName);
+      body.classList.remove(darkClassName);
+    }
+  }
+  isDarkMode() {
+    const storedTheme = localStorage.getItem('theme');
+    if (this.isDark) {
+      return true;
+    } else if (!this.isDark) {
+      return false;
+    } else {
+      return false; // Ajouter une instruction de retour pour ce cas
+    }
+  }
 }
